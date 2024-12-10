5742337370a9a8155960769808cac3a5a5c7e460
damies13 <damies13@gmail.com>
2024-12-11 00:18:14 +1000

    Initial layout

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import kivy
+kivy.require('2.3.0')
+
+# from glob import glob
+# from random import randint
+# from os.path import join, dirname
+from kivy.app import App
+from kivy.logger import Logger
+# from kivy.uix.scatter import Scatter
+# from kivy.properties import StringProperty
+
+
+class ImgSplitterApp(App):
+	pass
+
+
+if __name__ == '__main__':
+	ImgSplitterApp().run()
